a90d8fc25580e3e3f7390a57589ea4aac2e8b095
Camilo <kamitriana7@gmail.com>
2020-10-01 11:13:21 -0500

    END

--- Js/CargarMapa.js
+++ Js/CargarMapa.js
@@ -3,9 +3,9 @@ var latitudActual, longitudLugar;
 var longitudActual, latitudLugar;
 
 var options = {
-  enableHighAccuracy: true,
-  timeout: 6000,
-  maximumAge: 0
+    enableHighAccuracy: true,
+    timeout: 6000,
+    maximumAge: 0
 };
 
 navigator.geolocation.getCurrentPosition(success, error, options);
@@ -15,40 +15,43 @@ function success(position) {
 
     latitudActual = coordenadas.latitude;
     longitudActual = coordenadas.longitude;
-};
+}
+;
 function error(error) {
     console.warn('ERROR(' + error.code + '): ' + error.message);
-};
+}
+;
 
 function initMap() {
     latitudLugar = parseFloat(document.getElementById("lat").innerHTML);
     longitudLugar = parseFloat(document.getElementById("lon").innerHTML);
-    
+
     map = new google.maps.Map(document.getElementById('mapa'), {
         center: {lat: latitudActual, lng: longitudActual},
-        zoom: 16
+        zoom: 11
     });
     var marker = new google.maps.Marker({
         position: {lat: latitudActual, lng: longitudActual},
+        icon:'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
         map: map,
-        title: 'Ubicacion Actual'
+        title: 'UBICACION ACTUAL'
     });
     var marker2 = new google.maps.Marker({
         position: {lat: latitudLugar, lng: longitudLugar},
+        icon: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
         map: map,
-        title: 'Ubicacion Lugar'
+        title: 'DESTINO',
     });
     var flightPlanCoordinates = [
-        {lat:latitudActual, lng: longitudActual},
+        {lat: latitudActual, lng: longitudActual},
         {lat: latitudLugar, lng: longitudLugar}
     ];
     var flightPath = new google.maps.Polyline({
         path: flightPlanCoordinates,
         geodesic: true,
-        strokeColor: '#FF0000',
+        strokeColor: '#B40000',
         strokeOpacity: 1.0,
         strokeWeight: 2
     });
-
     flightPath.setMap(map);
 }
